02ff6f57230353165547539c51f09cd37dfab1c3
Thomas Alrek <thomas@alrek.no>
2016-03-14 16:05:23 +0100

    Separated this.connect to multiple named functions

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ module.exports = function(api_endpoint){
 
     var Client = require('node-rest-client').Client;
     var client = new Client();
-    
+        
     var colors = require("colors");
+    var async = require("async");
     
     var headers = {
         "Accept": "application/vnd.vegvesen.nvdb-v1+json", 
@@ -61,80 +62,81 @@ module.exports = function(api_endpoint){
         };
     }
     
+    function getChildNodes(key){
+        if(typeof _this[key].url !== 'undefined'){
+            _getResources(_this[key].url, function(child){
+                if(typeof child.ressurser !== 'undefined'){
+                    for(var i = 0; i < child.ressurser.length; i++){
+                        (function(index){
+                            var childNode = _toCamelCase(child.ressurser[i].rel);
+                            var url = child.ressurser[index].uri;
+
+                            var functionName = key + "." + childNode + "()";
+                            var deprecationMsg = colors.inverse(colors.cyan(functionName + " is deprecated"));
+
+                            var deprecated = false;
+
+                            if(typeof child.ressurser[i].status !== 'undefined'){
+                                if(child.ressurser[i].status === 'utgår'){
+                                    deprecated = true;
+                                }
+                            }
+
+                            if(/{.+?}/g.test(url)){
+                                _this[key][childNode] = function(params, callback){
+                                    if(deprecated){
+                                        console.log(deprecationMsg);
+                                    }
+                                    if(url.match(/{.+?}/g).length === 1){
+                                        url = url.replace(/{.+?}/g, function(){
+                                            return encodeURIComponent(_strEncode(params));
+                                        });
+                                    }else{
+                                        if(typeof params !== 'array' && typeof params !== 'object'){
+                                            console.log(colors.inverse(colors.yellow(functionName + ": multiple params must be array")));
+                                            return;
+                                        }
+                                        if(params.length < url.match(/{.+?}/g).length){
+                                            console.log(colors.inverse(colors.yellow(functionName + ": " + (url.match(/{.+?}/g).length - params.length) + " paramter(s) missing")));
+                                            return;
+                                        }
+                                        url.replace(/{.+?}/g, function(){
+                                            return encodeURIComponent(_strEncode(params.shift())); 
+                                        });    
+                                    }
+
+                                    _getResources(url, callback);
+                                };
+
+                            }else{
+                                _this[key][childNode] = function(callback){
+                                    if(deprecated){
+                                        console.log(deprecationMsg);
+                                    }
+                                    _getResources(url, callback);
+                                };
+                            }
+                            delete _this[key].url;
+                        })(i);
+                    };
+                };
+            });
+        };  
+    }
+    
     this.connect = function(callback){
                
         _getResources('/', function(root){
             if(typeof root.ressurser !== 'undefined'){
-                for(var i = 0; i < root.ressurser.length; i++){
-                    var rootNode = _toCamelCase(root.ressurser[i].rel);
-                    var rootUrl = root.ressurser[i].uri;
-                    _this[rootNode] = {};
-                    _this[rootNode].url = rootUrl;             
-                };
-
-                for(var key in _this){
-                    (function(key){
-                        if(typeof _this[key].url !== 'undefined'){
-                            _getResources(_this[key].url, function(child){
-                                if(typeof child.ressurser !== 'undefined'){
-                                    for(var i = 0; i < child.ressurser.length; i++){
-                                        (function(index){
-                                            var childNode = _toCamelCase(child.ressurser[i].rel);
-                                            var url = child.ressurser[index].uri;
-                                            
-                                            var functionName = key + "." + childNode + "()";
-                                            var deprecationMsg = colors.inverse(colors.cyan(functionName + " is deprecated"));
-                                            
-                                            var deprecated = false;
-                                            
-                                            if(typeof child.ressurser[i].status !== 'undefined'){
-                                                if(child.ressurser[i].status === 'utgår'){
-                                                    deprecated = true;
-                                                }
-                                            }
-                                            
-                                            if(/{.+?}/g.test(url)){
-                                                _this[key][childNode] = function(params, callback){
-                                                    if(deprecated){
-                                                        console.log(deprecationMsg);
-                                                    }
-                                                    if(url.match(/{.+?}/g).length === 1){
-                                                        url = url.replace(/{.+?}/g, function(){
-                                                            return encodeURIComponent(_strEncode(params));
-                                                        });
-                                                    }else{
-                                                        if(typeof params !== 'array' && typeof params !== 'object'){
-                                                            console.log(colors.inverse(colors.yellow(functionName + ": multiple params must be array")));
-                                                            return;
-                                                        }
-                                                        if(params.length < url.match(/{.+?}/g).length){
-                                                            console.log(colors.inverse(colors.yellow(functionName + ": " + (url.match(/{.+?}/g).length - params.length) + " paramter(s) missing")));
-                                                            return;
-                                                        }
-                                                        url.replace(/{.+?}/g, function(){
-                                                            return encodeURIComponent(_strEncode(params.shift())); 
-                                                        });    
-                                                    }
-                                                    
-                                                    _getResources(url, callback);
-                                                };
-
-                                            }else{
-                                                _this[key][childNode] = function(callback){
-                                                    if(deprecated){
-                                                        console.log(deprecationMsg);
-                                                    }
-                                                    _getResources(url, callback);
-                                                };
-                                            }
-                                            delete _this[key].url;
-                                        })(i);
-                                    };
-                                };
-                            });
-                        };
-                    })(key);
-                };
+                (function(){
+                    for(var i = 0; i < root.ressurser.length; i++){
+                        var rootNode = _toCamelCase(root.ressurser[i].rel);
+                        var rootUrl = root.ressurser[i].uri;
+                        _this[rootNode] = {};
+                        _this[rootNode].url = rootUrl;
+                        getChildNodes(rootNode);
+                    };    
+                })();
                 callback();
             }else{
                 throw colors.inverse(colors.red('Could not fetch resources from API endpoint'));;
