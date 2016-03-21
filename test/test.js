edb9c21df91857debe05f899601e7190f075f227
Thomas Alrek <thomas@alrek.no>
2016-03-21 17:16:50 +0100

    Updated tests

--- test/test.js
+++ test/test.js
@@ -1,14 +1,48 @@
 var assert = require('assert');
+var expect = require('chai').expect;
 var Vegvesen = require("../index.js");
 var vegvesen = new Vegvesen();
+var Helpers = require("../lib/helpers.js");
+
+describe('Helpers', function() {
+    describe('toCamelCase', function () {
+        it('"this-is-a-test#-æøå"', function () {
+            assert.equal(Helpers.toCamelCase("this-is-a-test#-æøå"), 'thisIsATestAoa');
+        });
+        it('"this_i s #&/$&$) -a-test-æøå"', function () {
+            assert.equal(Helpers.toCamelCase("this_i      s #&/$&$) -a-test-æøå"), 'thisisATestAoa');
+        });
+    });
+    describe('strEncode', function (){
+        it('Object', function () {
+            assert.equal(Helpers.strEncode({test: "object"}), '{"test":"object"}');
+        });
+        it('Number', function () {
+            assert.equal(Helpers.strEncode(5163), '5163');
+        });
+        it('Null', function () {
+            assert.equal(Helpers.strEncode(null), "null");
+        }); 
+        it('String', function () {
+            assert.equal(Helpers.strEncode("string"), "string");
+        }); 
+        it('Throw Error', function () {
+            expect(Helpers.strEncode.bind([0,1,2,3])).to.throw(Error);
+        });
+        it('Throw TypeError', function () {
+            expect(Helpers.strEncode).to.throw(TypeError);
+        });
+    });
+});
+
 describe('Vegvesen', function() {
 	describe('Constructor', function () {
-    	it('The new object should have a connect function', function () {
+    	it('Connect function', function () {
     		assert.equal(typeof vegvesen.connect, 'function');
     	});
   	});
 	describe('API objects', function () {
-    	it('After connect() the object should have 1 or more child objects', function () {
+    	it('API has child objects', function () {
     		vegvesen.connect(function(){
     			var objects = 0;
     			for(obj in vegvesen){
